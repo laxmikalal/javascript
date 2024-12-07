9b65c376b0ef69de98324b11c35598c5c3d117e4
Laxmi <51152166+laxmikalal@users.noreply.github.com>
2024-12-07 17:56:19 +0000

    datatype primitive

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,13 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
 let userEmail;
 
 const id = Symbol('123')
