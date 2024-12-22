ed469b452f03ff4be2f54d6bdae764a92f587af7
Laxmi <51152166+laxmikalal@users.noreply.github.com>
2024-12-22 10:45:01 +0000

    nums and math methods

---
+++ 01_basics/06_nums&math.js
@@ -0,0 +1,43 @@
+// Number 
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const newnum = 123.45
+console.log(newnum.toPrecision(4))
+
+const hundreads = 1000000000
+console.log(hundreads.toLocaleString('en-In'))
+
+// Maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(1,2,34,4));
+console.log(Math.max(0,1,2));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+//VVIMP
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
